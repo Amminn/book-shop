85bb494c4731517705b14650cbd64c27987aa880
Amminn <vrb.nnn@gmail.com>
2022-11-23 01:44:54 +0100

    feat: :construction: add popup feature with hide and show functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-  // createMain(),
-  // createFooter()
+let cartBooks = []
 
 window.onload = () => {
   const fragment = new DocumentFragment();
@@ -53,29 +52,10 @@ function shortText(text, maxLength) {
   return text.length >= maxLength ? text.slice(0, maxLength) + "..." : text
 }
 
+// pop out function
 
-// one book render
-function book(img, title, price, desc) {
-  const bookCart = document.createElement("div")
-  bookCart.className = "book"
-
-  bookCart.innerHTML = `
-    <img class="book-image" src="./assests/books-images/${img}.png" alt="Effective JavaScript: 68 Specific Ways book">
-    <div class="title-price">
-        <h3 class="book__title">${shortText(title, 30)}</h3>
-        <h3 class="book__price">${price}$</h3>
-      </div>
-      <p class="review"><img src="./assests/images/bxs-star.png" alt="star">4.6 / 534 review</p>
-      <p class="description">
-        ${shortText(desc, 158)}
-        <a class="readmore" href="">ReadMore</a>
-      </p>
-    <div class="buttons">
-      <button class="buttons__cart"><i class="ri-shopping-cart-line"></i></button>
-      <button class="buttons__buy">Buy Now <i class="ri-arrow-right-s-line"></i></button>
-    </div>`
-
-    return bookCart
+function openPopUp(id) {
+  return console.log(123)
 }
 
 function books() {
@@ -89,18 +69,83 @@ function books() {
   booksSectionTitle.textContent = "Please don't review the website yet am Still working on it"
   booksSectionTitle.className = "section-books__title"
 
+  const popUp = document.createElement("div")
+  popUp.className = "pop-out"
+
+  const popUpInner = document.createElement("div")
+  popUpInner.className = "pop-out-inner"
+
   const booksGrid = document.createElement("div")
   booksGrid.className = "books-grid"
 
-  // img, title, price, desc
-  fetch('./js/books.json') 
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(element => {booksGrid.append(book(element.imageLink, element.title, element.price, element.description))})
-  })
+  fetch('./js/books.json')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach((element, index) => {
+        booksGrid.insertAdjacentHTML('beforeend', `
+        <div class="book">
+          <img class="book-image" src="./assests/books-images/${element.imageLink}.png" alt="${element.title} book cover">
+          <div class="title-price">
+              <h3 class="book__title">${shortText(element.title, 30)}</h3>
+              <h3 class="book__price">${element.price}$</h3>
+            </div>
+            <p class="review"><img src="./assests/images/bxs-star.png" alt="star">4.6 / 534 review</p>
+            <p class="description">
+              ${shortText(element.description, 158)}
+              <a class="readmore" id="readmore" index="${index}" >ReadMore</a>
+            </p>
+          <div class="buttons">
+            <button class="buttons__cart"><i class="ri-shopping-cart-line"></i></button>
+            <button class="buttons__buy">Buy Now <i class="ri-arrow-right-s-line"></i></button>
+          </div>
+        </div>
+        `)
+      })
+      document.querySelectorAll('.readmore').forEach(item => {
+        item.addEventListener('click', (e) => {
+          let currentBook = data[e.target.getAttribute("index")]
 
-  container.append(booksSectionTitle, booksGrid)
-  booksSection.append(container)
+          popUpInner.innerHTML = `
+<div class="book-img">
+  <img src="./assests/books-images/${currentBook.imageLink}.png" alt="${currentBook.title} book cover">
+</div>
+<div class="text">
+  <button class="pop-out__close" id="popup-close"><i class="ri-close-line"></i></button>
+  <div class="top-side">
+    <p class="books__author">${currentBook.author}</p>
+    <h2 class="books__book-title">${currentBook.title}</h2>
+    <div class="books__review-price">
+      <div class="books__review">
+        <img src="./assests/images/bxs-star.png" alt="review star">
+        <p class="review"> 4.6 / 543 review</p>
+      </div>
+      <h3 class="number">${currentBook.price}$</h3>
+    </div>
+    <p class="description">${currentBook.description}</p>
+  </div>
+  <div class="pop-out__buttons">
+    <button class="fill-button">Buy Now <i class="ri-arrow-right-s-line"></i></button>
+    <button class="border-button">Add To Cart</button>
+  </div>
+</div>
+          `
+          booksSection.classList.add("pop-active", "blur")
+          popUp.append(popUpInner)
+          container.append(booksSectionTitle, popUp, booksGrid)
+          booksSection.append(container)
+
+          document.getElementById("popup-close").addEventListener('click', () => {
+            booksSection.classList.remove("pop-active", "blur")
+            popUpInner.remove()
+            popUp.remove()
+
+          })
+        })
+      })
+    })
+
+    container.append(booksSectionTitle, booksGrid)
+    booksSection.append(container)
 
   return booksSection
 }
